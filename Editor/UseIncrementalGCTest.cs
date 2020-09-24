010f734674b25b911fb90bbd94d7f7035d249a22
baba_s <shotababa1206@gmail.com>
2020-09-24 20:01:19 +0900

    first commit

---
+++ Editor/UseIncrementalGCTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class UseIncrementalGCTest
+	{
+		[Category( "Kogane" )]
+		[Test]
+		public void Incremental_GC_が有効になっているか()
+		{
+			Assert.IsTrue( PlayerSettings.gcIncremental );
+		}
+	}
+}
